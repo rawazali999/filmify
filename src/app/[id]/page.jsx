0bd458577ed7c49dd009bf3fd696b008b100fe26
Rawaz Ali <rawazali899@gmail.com>
2023-08-27 02:49:31 +0300

    Movie page design fix

--- src/app/[id]/page.jsx
+++ src/app/[id]/page.jsx
@@ -4,32 +4,40 @@ import Navbar from "../components/Navbar";
 import Image from "next/image";
 
 export default async function page({ params: { id } }) {
-  console.log(id);
   const movie = await getMoviesById(id);
   console.log(movie);
   return (
     <>
       <Navbar />
-      <div className="container mx-auto py-8">
-        <div className="flex flex-wrap">
-          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 px-4 mb-8">
+      <div
+        className=" w-full h-full bg-cover"
+        style={{
+          backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`,
+        }}
+      >
+        <div className="flex flex-col  sm:flex-row w-full backdrop-blur-lg h-full pt-4 ">
+          <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/3 px-4 mb-8">
             <Image
               src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
               width={200}
               height={300}
               alt="Movie Poster"
-              className="rounded-lg shadow-md"
+              className="rounded-lg shadow-md w-full h-auto"
             />
           </div>
-          <div className="w-full sm:w-1/2 md:w-2/3 lg:w-3/4 xl:w-4/5 px-4">
-            <h2 className="text-2xl font-semibold">{movie.title}</h2>
-            <p className="text-gray-700 mt-4">
-              {movie.overview}
-            </p>
-            <p className="text-gray-700 mt-4">
-              Director: John Doe <br />
-              Release Date: January 1, 2023 <br />
-              Genre: Action, Adventure
+          <div className="shadow w-full sm:w-1/2 md:w-2/3 lg:w-3/4 xl:w-4/5 px-4">
+            <a href={movie.homepage} className="text-2xl font-bold text-white ">
+              {movie.title}
+            </a>
+            <p className="text-gray-200 mt-4 ">{movie.overview}</p>
+            <p className="text-gray-200 mt-4">
+              Release Date: {movie.release_date} <br />
+              Genre: {movie.genres.map((genre) => genre.name).join(", ")} <br />
+              Rating: {movie.vote_average} <br />
+              <br />
+              Budget: {movie.budget.toLocaleString()} $ <br />
+              {/* add comma between each three digit to the revenue */}
+              Revenue: {movie.revenue.toLocaleString()} $ <br />
             </p>
           </div>
         </div>
